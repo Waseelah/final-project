a1adbf0f97db4b480d3db953bc46b22cf77e3e03
Waseelah <Wasilatayinla35@gmail.com>
2026-03-11 13:37:51 +0100

    new folder

---
+++ src/Pages/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import FirstSection from '../components/First';
+const LandingPage = () => {
+  return (
+    <div>
+      <FirstSection />
+    </div>
+  )
+}
+
+export default LandingPage
---
+++ src/components/First.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FirstSection = () => {
+  return (
+    <div >
+        <div className='w-797px'>
+            <h1 className='text-3xl font-bold text-[#141B34]'>
+                Send money across Africa. <span className='text-[#FFB700]'> Fast. Fair. Transparent. </span>
+            </h1>
+        </div>
+    </div>
+  )
+}
+
+export default FirstSection
