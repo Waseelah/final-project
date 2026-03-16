fcf61a28a1a463058d89ba88b9fd31f119f3df2f
Waseelah <Wasilatayinla35@gmail.com>
2026-03-16 15:26:38 +0100

    two new components

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -2,14 +2,22 @@ import React from 'react'
 import FirstSection from '../components/First';
 import Second from '../components/Second';  
 import BuiltForTrust from '../components/builtForTrust';
+import Work from '../components/Work';
 
 
+import Nav from '../components/Nav'
+import Transfer from '../components/Transfer';
 const LandingPage = () => {
   return (
     <div>
+      <Nav />
+
       <FirstSection />
       < Second/>
+        <Work />
       <BuiltForTrust/>
+      <Transfer />
+  
     </div>
   )
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+
+const Nav = () => {
+  return (
+    <nav className=" flex items-center justify-between md:px-10 sm:px-5 py-10 align-center">
+        <div className='w-15 h-7 '>
+            <img src='/src/assets/image.png' alt="Logo" />
+        </div>
+        <div className='hidden md:flex items-center gap-10 text-[13px] text-black' >
+            <p>How it works</p>
+            <p>Transperancy</p>
+            <p>Support</p>
+        </div>
+        <div className='flex items-center gap-8 text-[12px] text-[#111]'>
+            <button>Login</button>
+            <button>About</button>
+        </div>
+        <div className='md:hidden px-4 py-4 space-y-3 text-[13px] text-[#111]'>
+            <a href="#"className='block'>How it works</a>
+            <a href="#"className='block'>Transparency</a>
+            <a href="#"className='block'>Support</a>
+            <a href="#"className='block'>login</a>
+            <a href="#"className='block'>About</a>
+        </div>
+    </nav>
+
+  )
+}
+
+
+export default Nav
