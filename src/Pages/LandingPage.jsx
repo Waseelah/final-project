e0c732a15d2876fdd9842ab6f32783720190816f
spaceward4 <ibrahimsultanadeshina13@gmail.com>
2026-03-16 10:57:52 +0100

    new section

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import FirstSection from '../components/First';
+import Nav from '../components/Nav'
+import Transfer from '../components/Transfer';
+import Howitworks from '../components/Howitworks';
 const LandingPage = () => {
   return (
     <div>
+      <Nav />
+
       <FirstSection />
+      <Transfer />
+      <Howitworks />
     </div>
   )
 }
---
+++ src/components/BuiltForTrust.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { FaMoneyBillWave } from "react-icons/fa";
+import { IoEyeOutline } from "react-icons/io5";
+import { IoLocationOutline } from "react-icons/io5";
+import { IoAtCircleOutline } from "react-icons/io5";
+import { GrSecure } from "react-icons/gr";
+import { BiSupport } from "react-icons/bi";
+
+const BuiltForTrust = () => {
+    
+  return (
+    <div>BuiltForTrust</div>
+  )
+}
+
+export default BuiltForTrust
