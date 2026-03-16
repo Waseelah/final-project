4854502bd14d91ee9417b77d6aaed9833f4a9e93
Waseelah <Wasilatayinla35@gmail.com>
2026-03-16 12:16:49 +0100

    after pull

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import FirstSection from '../components/First';
+import Second from '../components/Second';  
+import BuiltForTrust from '../components/builtForTrust';
+
+
 const LandingPage = () => {
   return (
     <div>
       <FirstSection />
+      < Second/>
+      <BuiltForTrust/>
     </div>
   )
 }
